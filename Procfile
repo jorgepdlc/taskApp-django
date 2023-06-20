24b38fb862ac1443bf61b831f3ebfdc06a12b2c8
jorgepdlc <jprietodlc@usal.es>
2023-06-20 13:55:03 +0200

    requirements y Procfile
    para lanzar en railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && gunicorn django_crud_api.wsgi
--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useNavigationType } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 export function TaskCard({ task }) {
 
